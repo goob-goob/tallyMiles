ad8fd2eca904c045035b83c27beb792618b4b0a3
goob <gabriel.miller.developer@gmail.com>
2023-09-20 16:43:24 -0700

    ui updates

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,13 @@ function App() {
 
   const things = Object.entries(entries).map(([key, value]) => [Number(key), value]).sort(([k1, v1], [k2, v2]) => k2 - k1)
 
-  const sevenDaysEntries = things.filter(([key, value]) => key - MILLI_IN_SEVEN_DAYS)
+  const sevenDaysEntries = things.filter(([key, value]) => key - MILLI_IN_SEVEN_DAYS).slice(0, 7)
   // console.log('sevenDaysEntries', sevenDaysEntries)
   const sevenDaysValues = sevenDaysEntries.map(([key, value]) => Number(value))
   const sevenDaysTotal = sevenDaysValues.reduce((a, b) => a + b, 0)
   // console.log('sevenDaysTotal', sevenDaysTotal)
 
-  const thirtyDaysEntries = things.filter(([key, value]) => key - MILLI_IN_THIRTY_DAYS)
+  const thirtyDaysEntries = things.filter(([key, value]) => key - MILLI_IN_THIRTY_DAYS).slice(0, 30)
   // console.log('thirtyDaysEntries', thirtyDaysEntries)
   const thirtyDaysValues = thirtyDaysEntries.map(([key, value]) => Number(value))
   const thirtyDaysTotal = thirtyDaysValues.reduce((a, b) => a + b, 0)
@@ -58,34 +58,34 @@ function App() {
       </section>
       <section className='w-full h-1/2 flex text-3xl'>
         <section className='lg:w-1/4  h-full outline-teal-500'></section>
-        <section className='w-5/6 mx-16 lg:ml-0 lg:w-1/2 h-full  outline-yellow-400 flex flex-row items-center justify-center'>
-          <section className=' outline-green-500 flex flex-col w-1/2 lg:w-1/3 h-full pt-2'>
+        <section className='w-5/6 mx-16 lg:ml-0 lg:w-1/2 h-full  outline-yellow-400 flex flex-col-reverse lg:flex-row items-center justify-center'>
+          <section className=' outline-green-500 flex flex-col w-5/6 lg:w-1/3 h-full pt-2'>
             <section>
               {Object.entries(sevenDaysEntries).map(([index, [key, value]]) => (
 
                 <section
-                  className='h-10 bg-white text-black text-3xl my-2 rounded'
+                  className='lg:h-10 bg-white text-black text-6xl lg:text-3xl my-2 rounded-lg pl-4'
                   key={key}>
                   {String(value)}
                 </section>
               ))}
             </section>
             <form>
-              <input name='' id='goob-input' type="number" onChange={handleNumberChange} className=' mt-4 mb-3 rounded text-black w-full h-16 outline outline-red-300' />
+              <input name='' id='goob-input' type="number" onChange={handleNumberChange} className='pl-4 text-6xl mt-4 mb-3 rounded text-black w-full h-16 outline outline-red-300' />
             </form>
             <button
-              className='bg-teal outline-red-600 outline rounded'
+              className='bg-teal outline-red-600 outline rounded text-6xl'
               onClick={addEntry}
             >Add Entry
             </button>
             {/* <button className='bg-red outline-blue-500 outline h-6'>Other button</button> */}
           </section>
           <section className='outline-purple-500  w-1/2 lg:w-1/3 h-full'>
-            <section className='w-full h-1/6  outline-orange-500 pl-4 pt-4'>
+            <section className='w-full lg:h-1/6  outline-orange-500 mb-4 pl-4 pt-4 text-5xl'>
               <h1>Last 7 Days</h1>
               <p>{sevenDaysTotal}</p>
             </section>
-            <section className='w-full h-1/6  outline-orange-500 pl-4'>
+            <section className='w-full lg:h-1/6  outline-orange-500 pl-4 mb-4 text-5xl'>
               <h1>Last 30 Days</h1>
               <p>{thirtyDaysTotal}</p>
             </section>
